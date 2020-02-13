18ee499b2f38729a743d7597231073c3d4f886a9
Gavin <gerezuma@mills.edu>
2020-02-13 11:23:59 -0800

    commiting

--- share/models.py
+++ share/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+# Module 0
+# Script model
+class Script(models.Model):
+    title = models.CharField(max_length=30, null=False, blank=False, unique=True)
+    description = models.TextField(max_length=100, null=False, blank=False, unique=False)
+    # user can enter a a piece of code or an url
+    code = models.TextField(max_length=10000, unique=False)
+    url = models.URLField(unique=False, blank=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.title
