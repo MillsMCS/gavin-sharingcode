be657c5a3b11beb3c02ecd64f721c445c5b8f444
Gavin <gerezuma@mills.edu>
2020-02-13 12:24:47 -0800

    Module0 -2

--- share/admin.py
+++ share/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
+from .models import Script
+admin.site.register(Script)
 
-    from.models import Script
-    admin.site.register(Script)
 # Register your models here.
