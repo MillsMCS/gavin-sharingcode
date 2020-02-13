0c9a2a261cc981a8322f9ca1c34203f5c2f54366
Gavin <gerezuma@mills.edu>
2020-02-13 12:18:47 -0800

    Module 0

--- share/views.py
+++ share/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from share.models import Script
+# Module 0
 # Create your views here.
+def index(request):
+    if request.method == "GET":
+        return HttpResponse("Hello World!")
