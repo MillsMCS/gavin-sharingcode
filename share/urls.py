91e0de015027b4611ea3a5161304948b4f530d33
Gavin Erezuma <gerezuma@mills.edu>
2020-02-28 06:44:49 -0800

    Module3

--- share/urls.py
+++ share/urls.py
@@ -5,4 +5,17 @@ app_name = 'share'
 urlpatterns = [
     path('',views.index, name='index'), #Module 0
     path('first_script', views.get_first_script, name='first_script'), # Module 1
+
+    path('signup',views.signup, name='signup'),
+    path('create',views.create, name='create'),
+
+    path('login',views.login_view, name='login'),
+    path('loguser',views.login_user,name='loguser'),
+
+    path('logout',views.logout_view, name='logout'),
+
+    path('dashboard', views.dashboard_view, name='dashboard'),
+    path('publish_problem', views.publish_problem, name='publish_problem'),
+
+
 ]
--- share/views.py
+++ share/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from share.models import Script
+# Module 3 imports
+# import three functions: authentication, login, logout
+from django.contrib.auth import authenticate, login, logout
+
+# import redirect
+from django.shortcuts import render, redirect
+
+# import all the models created so far
+from .models import Script, Problem, Coder
+
+# import User model
+from django.contrib.auth.models import User
 
 
 # Module 0
@@ -9,13 +19,83 @@ from share.models import Script
 #     if request.method == "GET":
 #         return HttpResponse("Hello World!")
 
+
+#*************************************************
 # Module 1
 def get_first_script(request):
     if request.method == "GET":
         script = Script.objects.all()[0]
         return HttpResponse(str(script.title) + " " + str(script.description))
 
+
+#*************************************************
 # Module 2
 def index(request):
     if request.method == "GET":
         return render(request, 'share/index.html')  # new line
+
+
+#*************************************************
+# Module 3 Authentication functions
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect("share:index")
+    return render(request, 'share/signup.html')
+
+def create(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        coder_yet = request.POST['coder_yet_checkbox']
+
+        if username is not None and email is not None and password is not None: # checking that they are not None
+            if not username or not email or not password: # checking that they are not empty
+                return render(request, "share/signup.html", {"error": "Please fill in all required fields"})
+            if User.objects.filter(username=username).exists():
+                return render(request, "share/signup.html", {"error": "Username already exists"})
+            elif User.objects.filter(email=email).exists():
+                return render(request, "share/signup.html", {"error": "Email already exists"})
+            # save our new user in the User model
+            user = User.objects.create_user(username, email, password)
+            coder = Coder.objects.create(user= user, coder_yet = coder_yet).save()
+            user.save()
+
+            login(request, user, backend="django.contrib.auth.backends.ModelBackend")
+            # this logs in our new user, backend means that we are using the  Django specific auhentication and not 3rd party
+
+        return redirect("share:index")
+
+    else:
+        return redirect("share:signup")
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("share:index")
+    return render(request, 'share/login.html')
+
+# the function loguser is called from the login form
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        if not username or not password:
+            return render(request, "share/login.html", {"error":"One of the fields was empty"})
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("share:index")
+        else:
+            return render(request, "share/login.html", {"error":"Wrong username or password"})
+    else:
+        return redirect("share:index")
+
+def logout_view(request):
+    logout(request)
+    return redirect("share:login")
+
+def dashboard_view(request):
+    pass
+def publish_problem(request):
+    pass
