83b93e6747daa7c4a099e7d4bc2415826c9085b5
Gavin Erezuma <gerezuma@mills.edu>
2020-03-03 12:01:49 -0800

    Module4

--- share/urls.py
+++ share/urls.py
@@ -4,7 +4,7 @@ from . import views  #import everything from views module
 app_name = 'share'
 urlpatterns = [
     path('',views.index, name='index'), #Module 0
-    path('first_script', views.get_first_script, name='first_script'), # Module 1
+    # path('first_script', views.get_first_script, name='first_script'), # Module 1
 
     path('signup',views.signup, name='signup'),
     path('create',views.create, name='create'),
@@ -14,8 +14,11 @@ urlpatterns = [
 
     path('logout',views.logout_view, name='logout'),
 
-    path('dashboard', views.dashboard_view, name='dashboard'),
+    path('dashboard', views.dashboard, name='dashboard'),
     path('publish_problem', views.publish_problem, name='publish_problem'),
+    path('problem/<int:problem_id>/show', views.show_problem, name='show_problem'),
 
+    path('problem/<int:problem_id>/show_my_problem', views.show_my_problem, name='show_my_problem'),
+    path('script/<int:script_id>/show_my_script', views.show_my_script, name='show_my_script'),
 
 ]
