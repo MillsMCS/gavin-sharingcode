97b851ef0d57fb4351b5035640bac7461e6a0685
Gzuma <gavinereazuma@gmail.com>
2020-03-12 12:20:11 -0700

    Module6 done

--- share/urls.py
+++ share/urls.py
@@ -16,12 +16,12 @@ urlpatterns = [
 
     path('dashboard', views.dashboard, name='dashboard'),
 
-    # Problems
+    # Publish Problems
     path('publish_problem', views.publish_problem, name='publish_problem'),
+
+    # Show Problems
     path('problem/<int:problem_id>/show', views.show_problem, name='show_problem'),
     path('problem/<int:problem_id>/show_my_problem', views.show_my_problem, name='show_my_problem'),
-
-
 
     # scripts
     path('script/<int:script_id>/show_my_script', views.show_my_script, name='show_my_script'),
